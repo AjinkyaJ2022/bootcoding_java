23371a3b9f7868680b543db4ab61f273d8c9165d
AjinkyaJ2022 <ajinkya.r.junghare@gmail.com>
2022-09-19 23:02:52 +0530

    compile Java programs

---
+++ bootcoding_java/src/com/bootcoding/basic/Goodby.java
@@ -0,0 +1,8 @@
+package com.bootcoding.basic;
+
+public class Goodby {
+    public static void main(String[] args) {
+        System.out.print("Goodby, ");
+        System.out.println("Cruel world");
+    }
+}
---
+++ bootcoding_java/src/com/bootcoding/basic/Hello3.java
@@ -0,0 +1,8 @@
+package com.bootcoding.basic;
+
+public class Hello3 {
+    public static void main(String[] args) {
+        System.out.println("Hello!\nHow are you doing?\n");
+        System.out.println("She said \"Hello!\" to me");
+    }
+}
