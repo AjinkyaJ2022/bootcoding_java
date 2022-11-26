ed686098a35b10e4ae7a6616b3451de840b67748
AjinkyaJ2022 <ajinkya.r.junghare@gmail.com>
2022-11-26 13:25:05 +0530

    addition example

---
+++ bootcoding_java/src/com/bootcoding/basic/Addition.java
@@ -0,0 +1,13 @@
+package com.bootcoding.basic;
+
+public class Addition {
+    public static void main(String[] args) {
+        int n=5;
+        double m=1;
+        for (int i=0; i<=n;i++){
+            m=m+1/2;
+            System.out.println(n);
+        }
+
+    }
+}
