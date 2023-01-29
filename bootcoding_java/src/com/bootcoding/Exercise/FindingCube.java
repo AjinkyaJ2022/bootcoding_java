d8802d62bb69f85c24600298983b89aca92e4670
AjinkyaJ2022 <ajinkya.r.junghare@gmail.com>
2023-01-29 11:59:13 +0530

    exercise

---
+++ bootcoding_java/src/com/bootcoding/Exercise/FindingCube.java
@@ -0,0 +1,12 @@
+package com.bootcoding.Exercise;
+
+public class FindingCube {
+    public static void main(String[] args) {
+        int n=5;
+
+        for (int i=1;i<=n;i++){
+            System.out.println("Number is: "+i+" Cube of the"+i +"is: "+(i*i*i));
+        }
+
+    }
+}
